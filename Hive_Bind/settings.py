9bd57f8c6655be7177cf116d824c0a206858259d
thurstonr18 <55929023+thurstonr18@users.noreply.github.com>
2021-07-26 10:34:19 -0400

    Update settings.py

--- Hive_Bind/settings.py
+++ Hive_Bind/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '35y+_ts+dycx8*1she0gyx5hcu-s!(3i_gxdau0lnn97=d)&g='
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
